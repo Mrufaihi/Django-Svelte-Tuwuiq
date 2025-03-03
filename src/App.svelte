e71b4c25be8aa37a2a34d49a69168aa9f9f36df2
Mohammed Alrufaihi <malrufaihi@gmail.com>
2025-03-03 07:00:16 +0300

    init git

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Button from './lib/components/ui/button/button.svelte';
+  import * as Card from '$lib/components/ui/card/index';
+</script>
+
+<main>
+  <Button class="m-3">Click pls</Button>
+
+  <Card.Root class="justify-center items-center flex flex-col m-10 ">
+    <Card.Header>
+      <Card.Title>Title</Card.Title>
+      <Card.Description>Descript</Card.Description>
+    </Card.Header>
+    <Card.Content>
+      <p>Card content</p>
+    </Card.Content>
+    <Card.Footer>
+      <p>Card footer</p>
+    </Card.Footer>
+  </Card.Root>
+</main>
